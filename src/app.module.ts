2e1950ec89769b1ed2d37606b222ae01da536805
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-01-12 14:32:43 +0000

    setup liquidjs and modules pages - users created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PagesModule } from './pages/pages.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'public'),
+      renderPath: '/public/',
+    }),
+    UsersModule,
+    PagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
